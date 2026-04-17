e971b67e4cbadc495b185e0ea5c696735063eae7
tursunov-backent <donitursunov98@gmail.com>
2026-04-18 00:10:40 +0500

    main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
-from app.routers import books
+from .routers.books import router
+
+from .models import *
+from .database import engine
+
+Base.metadate.create_all(bind=engine)
 
 app = FastAPI(title="Book Management API")
 
-app.include_router(books.router)
+app.include_router(router)
